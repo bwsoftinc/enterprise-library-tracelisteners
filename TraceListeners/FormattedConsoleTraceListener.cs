74021eb784eafe7a45d16f3354aa6ccd48bbe101
bwsoftinc <bwapling@hotmail.com>
2016-12-18 00:33:04 -0600

    add formattedconsoletracelistener

---
+++ TraceListeners/FormattedConsoleTraceListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+
+namespace BWSoftInc.EnterpriseLogging.TraceListeners
+{
+    [ConfigurationElementType(typeof(CustomTraceListenerData))]
+    public class FormattedConsoleTraceListener : CustomTraceListener
+    {
+        public override void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+        {
+            var entry = data as LogEntry;
+
+            if (entry != null)
+            {
+                if (Formatter != null)
+                    WriteLine(Formatter.Format(entry));
+                else
+                    WriteLine(entry.Message);
+            }
+            else
+            {
+                base.TraceData(eventCache, source, eventType, id, data);
+            }
+        }
+    }
+}
